e8f38a1c22fdeb9a757f9bddf09a6c4eb03145c2
Christopher Wood <cjwood032@gmail.com>
2018-06-24 19:57:40 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,32 +1,30 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
-   student_hash.each do |spec, value| 
-   	self.send ("#{spec}=", value)
-   	end #end the each-do
-   	@@all << self
-  end #end the initialize method
+    student_hash.each do |attribute, value|
+      self.send("#{attribute}=", value)
+    end
+    @@all << self
+  end
 
   def self.create_from_collection(students_array)
-    students_array.each do |student_hash| # for each object in the array
-    	Student.new(student_hash) #creates a new student with the object
-    end #end the each do
-  end#end the create method
+    students_array.each do |student_hash|
+      Student.new(student_hash)
+    end
+  end
 
   def add_student_attributes(attributes_hash)
-     attributes_hash.each do |attr, value| #from the hash get each attribute and value
-      self.send("#{attr}=", value) #send the studen tthe new attribute/value key pair
-    end #end the each-do
-    self #call self
-  end #end the add attributes method
+    attributes_hash.each do |attr, value|
+      self.send("#{attr}=", value)
+    end
+    self
+  end
 
   def self.all
     @@all
-  end#end the self all
-end #end the student class
-
+  end
 end
